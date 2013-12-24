2db50bbe5ac09018443c79c8ab9105dca3222ad5
justinek <justinek@stanford.edu>
2013-12-24 07:54:47 -0800

    launched feature exp

--- mTurkScripts/FeatureExp/Launch/features-getResults.sh
+++ mTurkScripts/FeatureExp/Launch/features-getResults.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 pushd /Applications/aws-mturk-clt-1.3.0/bin
-./getResults.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -successfile /Users/justinek/Documents/Grad_school/Research/Metaphor/mTurkScripts/FeatureExp/Launch//features.success -outputfile /Users/justinek/Documents/Grad_school/Research/Metaphor/mTurkScripts/FeatureExp/Launch//features.results
+./getResults.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -successfile /Users/justinek/Documents/Grad_school/Research/Metaphor/metaphors/mTurkScripts/FeatureExp/Launch/features.success -outputfile /Users/justinek/Documents/Grad_school/Research/Metaphor/metaphors/mTurkScripts/FeatureExp/Launch/features.results
 popd
